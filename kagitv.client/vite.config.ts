4b17e87a9a7668530f6555831b8743235ee8ea5d
anton <antonius.ivan.han@gmail.com>
2026-01-09 09:03:02 +0700

    change to APP

--- kagitv.client/vite.config.ts
+++ kagitv.client/vite.config.ts
@@ -42,7 +42,7 @@ export default defineConfig({
     plugins: [plugin()],
     resolve: {
         alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url))
+            '@': fileURLToPath(new URL('./app', import.meta.url))
         }
     },
     server: {
